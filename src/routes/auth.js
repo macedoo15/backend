896a15fb34f559463af1979dd4de6c07159f8cdc
macedo <ds091339@gmail.com>
2026-07-19 21:08:31 -0300

    FIX

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,8 +6,8 @@ const router     = express.Router();
 const controller = require('../controllers/authController');
 const { requireAuth } = require('../middlewares/auth');
 
-router.post('/login',   controller.fazerLogin);                    // POST /api/login
-router.post('/logout',  requireAuth, controller.fazerLogout);      // POST /api/logout
-router.get('/session',  controller.verificarSessao);               // GET  /api/session
+router.post('/login',   controller.fazerLogin);                           // POST /api/login
+router.post('/logout',  requireAuth, controller.fazerLogout);             // POST /api/logout
+router.get('/session',  requireAuth, controller.verificarSessao);         // GET  /api/session
 
 module.exports = router;
